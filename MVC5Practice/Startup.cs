4261e513fd3944ee9b6f1e5142ff6f84b48c0b12
ycwu <wuik2132@yahoo.com.tw>
2016-11-12 15:19:58 +0800

    專案建立

---
+++ MVC5Practice/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5Practice.Startup))]
+namespace MVC5Practice
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
