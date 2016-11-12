f61b2f98fd901937f5882453d5eca5cb9e950fd3
ycwu <wuik2132@yahoo.com.tw>
2016-11-12 20:50:50 +0800

    17 練習透過 Entity Framework 查詢資料、建立資料、刪除資料、取出單筆資料

---
+++ MVC5Practice/Controllers/EFController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Practice.Models;
+
+namespace MVC5Practice.Controllers
+{
+    public class EFController : Controller
+    {
+        private FabricsEntities db = new FabricsEntities();
+
+        // GET: EF
+        public ActionResult Index()
+        {
+            var data = db.Product.Where(p => p.ProductName.Contains("White"));
+            return View(data.OrderByDescending(p => p.ProductId).Take(10));
+        }
+
+        // GET: EF/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Product.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        public ActionResult Create()
+        {
+            var product = new Product()
+            {
+                ProductName = "White Cat",
+                Active = true,
+                Price = 199,
+                Stock = 5
+            };
+            db.Product.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: EF/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Product.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: EF/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductId,ProductName,Price,Active,Stock")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        // GET: EF/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Product.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: EF/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Product.Find(id);
+            db.Product.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
