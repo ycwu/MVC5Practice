58b4736cc694c2f5c737f561d5b01746a03d55c0
ycwu <wuik2132@yahoo.com.tw>
2016-11-15 21:26:19 +0800

    40 練習 FileResult 的使用，顯示或下載一個圖檔

---
+++ MVC5Practice/Controllers/ARController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Text;
+
+namespace MVC5Practice.Controllers
+{
+    public class ARController : Controller
+    {
+        // GET: AR
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+        public ActionResult PartialViewTest()
+        {
+            return PartialView();
+        }
+
+        public ActionResult ContentTest()
+        {
+            return Content("開發實戰", "text/plain", Encoding.GetEncoding("Big5"));
+        }
+
+        public ActionResult FileTest()
+        {
+            // C:\Projects\MVC5Course\MVC5Course\Content\maxresdefault.jpg
+
+            var filePath = Server.MapPath("~/Content/maxresdefault.jpg");
+
+            return File(filePath, "image/jpeg");
+        }
+        public ActionResult FileTest2()
+        {
+            var filePath = Server.MapPath("~/Content/maxresdefault.jpg");
+
+            return File(filePath, "image/jpeg", "PPAP.jpg");
+        }
+
+    }
+}
