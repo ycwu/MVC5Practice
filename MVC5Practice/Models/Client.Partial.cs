1c099b044d0e07cd47365722ba915d2af1911b14
ycwu <wuik2132@yahoo.com.tw>
2016-11-20 22:24:55 +0800

    54 練習在特定模型類別下的某個屬性設定 [UIHint] 自訂 DisplayTemplates 與 EditorTemplates

--- MVC5Practice/Models/Client.Partial.cs
+++ MVC5Practice/Models/Client.Partial.cs
@@ -25,7 +25,11 @@ namespace MVC5Practice.Models
         
         [StringLength(1, ErrorMessage="欄位長度不得大於 1 個字元")]
         public string Gender { get; set; }
+
+        [DataType(DataType.Date)]
         public Nullable<System.DateTime> DateOfBirth { get; set; }
+
+        [UIHint("CreditRating")]
         public Nullable<double> CreditRating { get; set; }
         
         [StringLength(7, ErrorMessage="欄位長度不得大於 7 個字元")]
