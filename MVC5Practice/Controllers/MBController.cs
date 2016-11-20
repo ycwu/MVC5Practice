748d4ee80d448fd8b0ca1d9e595410a97d63ef8d
ycwu <wuik2132@yahoo.com.tw>
2016-11-20 10:46:39 +0800

    43 練習複雜模型繫結並可一次傳入多筆複雜模型資料繫結到陣列裡

--- MVC5Practice/Controllers/MBController.cs
+++ MVC5Practice/Controllers/MBController.cs
@@ -7,7 +7,7 @@ using MVC5Practice.Models.ViewModels;
 
 namespace MVC5Practice.Controllers
 {
-    public class MBController : Controller
+    public class MBController : BaseController
     {
         // GET: MB
         public ActionResult Index()
@@ -39,6 +39,30 @@ namespace MVC5Practice.Controllers
 
         public ActionResult MyFormResult()
         {
+            return View();
+        }
+
+        public ActionResult ProductList()
+        {
+            var data = db.Product.OrderBy(p => p.ProductId).Take(10);
+            return View(data);
+        }
+
+        public ActionResult BatchUpdate(ProductBatchUpdateViewModel[] items)
+        {
+            if (ModelState.IsValid)
+            {
+                foreach (var item in items)
+                {
+                    var product = db.Product.Find(item.ProductId);
+                    product.ProductName = item.ProductName;
+                    product.Active = product.Active;
+                    product.Price = item.Price;
+                    product.Stock = item.Stock;
+                }
+                db.SaveChanges();
+                return RedirectToAction("ProductList");
+            }
             return View();
         }
     }
---
+++ MVC5Practice/Models/ViewModels/ProductBatchUpdateViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MVC5Practice.Models.ViewModels
+{
+    public class ProductBatchUpdateViewModel
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public Nullable<decimal> Price { get; set; }
+        public Nullable<bool> Active { get; set; }
+        public Nullable<decimal> Stock { get; set; }
+    }
+}
